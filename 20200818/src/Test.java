2e272860f28f38343342b9808d3226d1190b2834
feng_chen6 <249841036@qq.com>
2020-08-18 13:44:18 +0800

    数组

---
+++ 20200818/src/Test.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Test {
+     
+}
+/*    //写一个方法，将原来数组的所有元素都*2
+public static int[] fun(int[] arr){
+    for (int i=0;i<arr.length;i++) {
+       arr[i]=arr[i]*2;
+    }
+    return arr;
+}
+
+    public static void main(String[] args) {
+        int []arr={1,2,3};
+        System.out.println(Arrays.toString(fun(arr)));
+    }
+}*/
+/*    //遍历数组的三种方式
+    public static void main(String[] args) {
+
+        //方式1
+      int[] arr={1,2,3,4,5,6};
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        //方式2
+        for (int x:arr) {
+            System.out.print(x+" ");
+        }
+        System.out.println();
+        //方式3
+        System.out.println(Arrays.toString(arr));
+    }
+}*/
