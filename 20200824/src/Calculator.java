14878dc43f6a60b864265cc0694bfbb0f4f350c3
feng_chen6 <249841036@qq.com>
2020-08-24 15:37:26 +0800

    类和对象

---
+++ 20200824/src/Calculator.java
@@ -0,0 +1,39 @@
+public class Calculator {
+    private int num1;
+    private int num2;
+    public Calculator(){
+
+    }
+    public Calculator(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+    public int add(){
+        return this.num1+this.num2;
+    }
+    public int sub(){
+        return this.num1-this.num2;
+    }
+    public int mul(){
+        return this.num1*this.num2;
+    }
+    public double div(){
+        return this.num1*1.0/this.num2;
+    }
+}
---
+++ 20200824/src/Test.java
@@ -0,0 +1,53 @@
+public class Test{
+    public static void main(String[] args) {
+
+    }
+}
+//交换两个数字的值
+/*class MyValue{
+    private int val;
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
+public class Test {
+    public static void swap(MyValue myValue1,MyValue myValue2){
+        int temp=myValue1.getVal();
+        myValue1.setVal(myValue2.getVal());
+        myValue2.setVal(temp);
+
+    }
+    public static void main(String[] args) {
+        MyValue myValue1=new MyValue();
+        myValue1.setVal(10);
+        MyValue myValue2=new MyValue();
+        myValue2.setVal(20);
+        System.out.print(myValue1.getVal()+" "+myValue2.getVal());
+        swap(myValue1,myValue2);
+        System.out.println();
+        System.out.print(myValue1.getVal()+" "+myValue2.getVal());
+
+
+    }
+}*/
+    /*public static void main(String[] args) {
+        Calculator calculator=new Calculator();
+        calculator.setNum1(10);
+        calculator.setNum2(20);
+        System.out.println(calculator.add());
+        System.out.println(calculator.sub());
+        System.out.println(calculator.mul());
+        System.out.println(calculator.div());
+        System.out.println("----------------------------------");
+        Calculator calculator1=new Calculator(10,20);
+        System.out.println(calculator1.add());
+        System.out.println(calculator1.sub());
+        System.out.println(calculator1.mul());
+        System.out.println(calculator1.div());
+    }
+}*/
